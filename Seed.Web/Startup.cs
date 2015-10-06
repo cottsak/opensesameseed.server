11bb48c17367744bb02c9edcb96c97989daa0a47
matt kocaj <mkocaj@hotmail.com>
2015-10-06 14:40:24 +0800

    initial commit. basic MVC/API site using Owin/Startup.cs

---
+++ Seed.Web/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using System.Web.Routing;
+using Microsoft.Owin;
+using Owin;
+using Seed.Web;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace Seed.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
